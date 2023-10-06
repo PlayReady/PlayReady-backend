a55c01aa97411e2f4a7de54e72904a322639c314
ENIGMA <stanborst@gmail.com>
2023-10-06 02:20:37 +0200

    added repos for user and role

---
+++ src/main/java/com/playready/PlayReadyBackend/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.playready.PlayReadyBackend.repository;
+
+import com.playready.PlayReadyBackend.model.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role,String> {
+}
