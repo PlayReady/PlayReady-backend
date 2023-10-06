edf07d2ff128e479aea52943337df9be157e2c1a
ENIGMA <stanborst@gmail.com>
2023-10-06 02:20:07 +0200

    added entities for user and role

--- src/main/java/com/playready/PlayReadyBackend/model/Role.java
+++ src/main/java/com/playready/PlayReadyBackend/model/Role.java
@@ -1,4 +1,25 @@
 package com.playready.PlayReadyBackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+import java.util.Collection;
+
+@Entity
+@Table(name ="roles")
 public class Role {
+    @Id
+    private String rolename;
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users;
+
+    public String getRolename() {
+        return rolename;
+    }
+
+    public void setRolename(String rolename) {
+        this.rolename = rolename;
+    }
 }
--- src/main/java/com/playready/PlayReadyBackend/model/User.java
+++ src/main/java/com/playready/PlayReadyBackend/model/User.java
@@ -1,4 +1,39 @@
 package com.playready.PlayReadyBackend.model;
 
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    private String username;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
 }
