ec264464f9b5d645363635cfcd34919ef296801e
ENIGMA <stanborst@gmail.com>
2023-10-07 01:55:45 +0200

    added token to response body

--- src/main/java/com/playready/PlayReadyBackend/controler/AuthController.java
+++ src/main/java/com/playready/PlayReadyBackend/controler/AuthController.java
@@ -39,7 +39,7 @@ public class AuthController {
 
                 return ResponseEntity.ok()
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
-                        .body("Token generated");
+                        .body(token);
             }
             catch (AuthenticationException ex) {
                 return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
