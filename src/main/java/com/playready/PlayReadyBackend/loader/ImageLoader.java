d44c7e1ddcc42136ea6be7d8215bee13c34f1171
ENIGMA <stanborst@gmail.com>
2023-10-14 16:35:14 +0200

    added startup data

---
+++ src/main/java/com/playready/PlayReadyBackend/loader/ImageLoader.java
@@ -0,0 +1,34 @@
+package com.playready.PlayReadyBackend.loader;
+
+import com.playready.PlayReadyBackend.model.Product;
+import com.playready.PlayReadyBackend.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.util.Optional;
+
+@Component
+public class ImageLoader implements ApplicationRunner {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public void run(ApplicationArguments args) throws Exception {
+        ClassPathResource resource = new ClassPathResource("static/images/product.png");
+        byte[] imageBytes = Files.readAllBytes(resource.getFile().toPath());
+
+        // Load and update the image for the first three products
+        for (long productId = 1L; productId <= 3L; productId++) {
+            Optional<Product> productOptional = productRepository.findById(productId);
+            productOptional.ifPresent(product -> {
+                product.setImage(imageBytes);
+                productRepository.save(product);
+            });
+        }
+    }
+
+}
