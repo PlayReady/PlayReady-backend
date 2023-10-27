3241cbde79cbdaaab7670b1f39fec70d0116403f
ENIGMA <stanborst@gmail.com>
2023-10-27 14:54:27 +0200

    added invoice endpoint

---
+++ src/main/java/com/playready/PlayReadyBackend/dto/InvoiceDto.java
@@ -0,0 +1,13 @@
+package com.playready.PlayReadyBackend.dto;
+
+import java.time.Month;
+import java.time.Year;
+
+public class InvoiceDto {
+    public long id;
+    public Year year;
+    public Month month;
+    public double price;
+    public boolean paid;
+    public Long contractId;
+}
--- src/main/java/com/playready/PlayReadyBackend/service/ServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package com.playready.PlayReadyBackend.service;
-
-
-public interface ServiceInterface<Entity,Dto> {
-    Dto convertToDto(Entity entity);
-    Entity convertToEntity(Dto dto);
-}
